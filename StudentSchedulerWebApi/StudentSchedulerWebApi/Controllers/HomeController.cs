c2afab50c01c9bc82a63abd38e85fa73a0b61e29
Thomas Tramp <47607041+TomT15@users.noreply.github.com>
2022-10-07 14:23:15 -0500

    adding bootstrap and modal

--- StudentSchedulerWebApi/StudentSchedulerWebApi/Controllers/HomeController.cs
+++ StudentSchedulerWebApi/StudentSchedulerWebApi/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ namespace StudentSchedulerWebApi.Controllers
 {
     public class HomeController : Controller
     {
-        
         public IActionResult Index()
         {
             return View();
